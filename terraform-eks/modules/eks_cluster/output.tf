370b882ce2ebf6237213fb17a4206bf2e49a7e9c
gabrielthecoder <jorgegabriel.ti@gmail.com>
2025-06-04 09:01:06 -0300

    [update] module eks_cluster

--- terraform-eks/modules/eks_cluster/output.tf
+++ terraform-eks/modules/eks_cluster/output.tf
@@ -22,3 +22,13 @@ output "cluster_security_group_id" {
   description = "O ID do Security Group do cluster EKS."
   value       = aws_security_group.cluster_sg.id
 }
+
+output "cluster_security_group_id" {
+  description = "O ID do Security Group principal do cluster EKS (gerenciado pelo EKS)."
+  value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_shared_node_security_group_id" {
+  description = "O ID do Security Group compartilhado entre os nós do cluster."
+  value       = aws_security_group.cluster_shared_node_sg.id
+}
